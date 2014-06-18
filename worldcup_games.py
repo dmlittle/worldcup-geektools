683b8c18843c01c34e35f1efa24408afdee131e9
Donald Little <dmlittle@mit.edu>
2014-06-18 13:17:13 -0700

    fixed timezone hours and added my timezone config

--- worldcup_games.py
+++ worldcup_games.py
@@ -7,6 +7,11 @@ from prettytable import PrettyTable
 from dateutil import parser
 from datetime import datetime
 
+
+########### CONFIG ###########
+my_timezone = "US/Pacific"
+##############################
+
 def fetch_data():
     url = 'http://worldcup.sfg.io/matches/today/'
 
@@ -36,14 +41,39 @@ def parse(json_data):
         combinedScore = str(homeGoals) + "-" + str(awayGoals)
 
         status = matches[i]['status']
-        game_time = parser.parse(matches[i]['datetime']).replace(tzinfo = pytz.timezone('US/Pacific'))
+        game_time = parser.parse(matches[i]['datetime'])
+
+        if matches[i]['location'] == "Maracan\u00e3 - Est\u00e1dio Jornalista M\u00e1rio Filho":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
+        elif matches[i]['location'] == "Arena Amazonia":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Manaus'))
+        elif matches[i]['location'] == "Estadio Mineirao":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
+        elif matches[i]['location'] == "Estadio Nacional":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
+        elif matches[i]['location'] == "Arena Pantanal":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Cuiaba'))
+        elif matches[i]['location'] == "Arena da Baixada":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
+        elif matches[i]['location'] == "Estadio Castelao":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Fortaleza'))
+        elif matches[i]['location'] == "Estadio das Dunas":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Fortaleza'))
+        elif matches[i]['location'] == "Estadio Beira-Rio":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
+        elif matches[i]['location'] == "Arena Pernambuco":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Recife'))
+        elif matches[i]['location'] == "Arena Fonte Nova":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Bahia'))
+        elif matches[i]['location'] == "Arena Corinthians":
+            game_time = game_time.replace(tzinfo = pytz.timezone('America/Sao_Paulo'))
 
         if status == "completed":
             status = "DONE"
         elif status == "future":
-            status = game_time.strftime("%I:%M %p")
+            status = game_time.astimezone(pytz.timezone(my_timezone)).strftime("%I:%M %p")
 
-        if datetime.now() >= game_time.replace(tzinfo=None):
+        if matches[i]['status'] != "future":
             if homeGoals > awayGoals:
                 homeTeam = '\033[92m' + homeTeam + '\033[0m'
                 awayTeam = '\033[91m' + awayTeam + '\033[0m'
@@ -54,9 +84,7 @@ def parse(json_data):
                 homeTeam = '\033[93m' + homeTeam + '\033[0m'
                 awayTeam = '\033[93m' + awayTeam + '\033[0m'
 
-
         table.add_row([homeTeam, awayTeam, combinedScore, status])
-
 
     return table
 
